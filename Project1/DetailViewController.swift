44b009980ce6aa022b9e0ecbb831fe4d9c392792
koraytacus <korayurun07@gmail.com>
2025-08-06 23:59:23 +0300

    I examine and rewrite HackingWithSwift' UIKit project 4.In this project I worked with WebKit

--- Project1/DetailViewController.swift
+++ Project1/DetailViewController.swift
@@ -17,6 +17,9 @@ class DetailViewController: UIViewController {
         title = selectedImage
         navigationItem.largeTitleDisplayMode = .never
         
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
+
+        
         if let imageToLoad = selectedImage {
             imageView.image = UIImage(named : imageToLoad)
         }
@@ -34,15 +37,18 @@ class DetailViewController: UIViewController {
         navigationController?.hidesBarsOnTap = false
     }
     
+    @objc func shareTapped() {
+        guard let image = imageView.image?.jpegData(compressionQuality: 0.8) else {
+            print("No image found")
+            return
+        }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        let vc = UIActivityViewController(activityItems: [image], applicationActivities: [])
+        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(vc, animated: true)
     }
-    */
+    
+
+  
 
 }
