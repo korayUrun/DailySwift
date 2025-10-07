fd3da5db6b4367abaa75e94bad4cc3a7ed971845
Koray <150171299+korayUrun@users.noreply.github.com>
2025-10-07 19:07:53 +0300

    I work on some data operation in SwiftUI

---
+++ DataOperationSwiftUI/VerilerSwiftUI/IkinciView.swift
@@ -0,0 +1,18 @@
+//
+//  IkinciView.swift
+//  VerilerSwiftUI
+//
+//  Created by Koray Urun on 6.10.2025.
+//
+
+import SwiftUI
+
+struct IkinciView: View {
+    var body: some View {
+        Text("Ikinci View")
+    }
+}
+
+#Preview {
+    IkinciView()
+}
---
+++ DataOperationSwiftUI/VerilerSwiftUI/VerilerSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  VerilerSwiftUIApp.swift
+//  VerilerSwiftUI
+//
+//  Created by Koray Urun on 6.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct VerilerSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            YapilacakView()
+        }
+    }
+}
